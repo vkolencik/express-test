f3226d3a1b65fc639c1f912657218ba2a7bd5d04
vkolencik <vojtech.kolencik@vistaprint.com>
2021-04-25 22:11:14 +0200

    Add bogus implementation

--- src/server.js
+++ src/server.js
@@ -1,9 +1,12 @@
 import fetch from 'node-fetch'
 
 exports.handler = async function (event, context) {
-  const uuid = await fetch('https://www.uuidgenerator.net/api/guid')
+
+  const response = await fetch('https://www.uuidgenerator.net/api/guid')
+
+  const name = event.queryStringParameters['name'] || 'world'
   return {
     statusCode: 200,
-    body: `Hello, world! {${uuid}}`
+    body: `Hello, ${name}! Your unique GUID is: {${response.data}}`
   };
 }
