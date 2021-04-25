a037c225c1d7f9d5145a7d6d0815a8c7068d4c4d
vkolencik <vojtech.kolencik@vistaprint.com>
2021-04-25 20:14:14 +0200

    Add netlify serverless boilerplate

--- index.js
+++ index.js
@@ -1,14 +1,5 @@
-const express = require('express')
-const serverless = require('serverless-http')
-const faker = require('faker')
-const app = express()
+'use strict';
 
-app.get('/', (req, res) => {
-  res.set('Content-Type', 'text/plain');
-  res.send(`Hello, ${faker.commerce.color()} ${faker.commerce.product().toLowerCase()}!`);
-})
+const app = require('./express/server');
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`listening on ${PORT}`));
-
-module.exports.handler = serverless(app);
+app.listen(3000, () => console.log('Local app listening on port 3000!'));
