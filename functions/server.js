7609de4b8d4816a70db91b709866ce8313531565
vkolencik <vojtech.kolencik@vistaprint.com>
2021-04-25 20:37:38 +0200

    Remove import

--- functions/server.js
+++ functions/server.js
@@ -1,8 +1,6 @@
-import faker from 'faker'
-
 exports.handler = async function (event, context) {
   return {
     statusCode: 200,
-    body: `Hello, ${faker.commerce.color()} ${faker.commerce.product().toLowerCase()!"
+    body: `Hello, world ${new Date.toString()}`
   };
 }
