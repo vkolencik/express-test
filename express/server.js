e8ec96773cc091cb917a22d369d6be4c79649f2f
vkolencik <vojtech.kolencik@vistaprint.com>
2021-04-25 20:21:07 +0200

    Add router

--- express/server.js
+++ express/server.js
@@ -1,13 +1,17 @@
 const express = require('express')
 const serverless = require('serverless-http')
 const faker = require('faker')
+
 const app = express()
+const router = express.Router();
 
 app.get('/', (req, res) => {
+  res.status(200);
   res.set('Content-Type', 'text/plain');
   res.send(`Hello, ${faker.commerce.color()} ${faker.commerce.product().toLowerCase()}!`);
+  res.end();
 })
-
+app.use('/.netlify/functions/server', router);  // path must route to lambda
 
 module.exports = app;
 module.exports.handler = serverless(app);
